c224b41659c0e0b1a74b3096db1656ff66804538
bsood01 <bhaveshsood14@gmail.com>
2024-02-16 12:40:56 +0000

    Sanity checks on login

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+import re
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)  
 
 # Temporary hard-coded constants for development purposes only
-uri = 'blah blah blah'
+uri ='blah blah blah'
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
@@ -28,14 +30,48 @@ def hello_world():
 @app.route('/submit', methods=['POST'])
 def submit_form():
     data = request.json
+
+    
     db = client.MusiCollab  
     users_collection = db.users  
+    #check_signup(data, users_collection)
+    email = data.get('email')
+    #password = data.get()
+    username = data.get('username')
+    dob = data.get('dateOfBirth')
+        
+    #check email validity
+
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return jsonify({'error': 'Invalid Email'})
+    if users_collection.find_one({ 'email': email }) is not None:
+        return jsonify({'error': 'Account Already Exists'})
+    
+    if users_collection.count_documents({ 'username': username }, limit = 1) != 0:
+          return jsonify({'error': 'Username Taken'})
+    
+    try:
+        date_obj = datetime.fromisoformat(dob[:-1])
+    except ValueError:
+        return jsonify({'error': 'Select a valid date'})
+
+    #d_str=date_obj.strftime('%Y-%m-%d %H:%M:%S') #print(d_str)
+    age = datetime.now() - date_obj
+    years = age.days // 365
+    # Check if the age is over 16
+    if (years <= 16):
+        return jsonify({'error': 'Must be 16 or over to sign up'})
 
     # insert the date into the DB
     result = users_collection.insert_one(data)
     
     # Success message if data was transferred successfully
     return jsonify({"success": True, "id": str(result.inserted_id)})
+       
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
