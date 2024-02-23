84add88d05437a6b9d5f2ddf7239dd3188ddba39
Fenlo <55544169+JamesFenzy09@users.noreply.github.com>
2024-02-23 17:42:36 +0000

    landing page created. both login and register pages works. successful path through both brings to blank profile page (for the minute)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import App from "./App.vue"; // This import should be at the top
 
 // Vuetify
 import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css"; // Import Material Design Icons
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
@@ -15,6 +16,9 @@ import router from "./router";
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    iconfont: "mdi", // Specify the iconfont to use, 'mdi' in this case
+  },
 });
 
 const app = createApp(App);
---
+++ frontend/src/views/LandingPage.vue
@@ -0,0 +1,42 @@
+<script>
+import { ref, computed } from "vue";
+import axios from "axios";
+
+export default {
+  name: "LandingPage",
+
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="landingPage">
+    <h1>Welcome to MusiCollab!</h1>
+    <p>
+      If you have an account,
+      <router-link to="/sign-in">sign in here</router-link>.
+    </p>
+    <p>Alternatively, <router-link to="/sign-up">sign up here</router-link>.</p>
+  </div>
+</template>
+
+<style scoped>
+.landingPage {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+}
+
+.landingPage h1 {
+  margin-bottom: 20px;
+}
+
+.landingPage p {
+  margin-bottom: 10px;
+}
+</style>
